52e9bb0991d9c2bd95af760c21c4f22e7ad30bf9
Manuela320 <manuelaandricioaei63@gmail.com>
2024-09-08 15:47:48 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
     <div>
