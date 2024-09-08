549f31d19a6610b1c55555d4c1723879ba13a142
Manuela320 <manuelaandricioaei63@gmail.com>
2024-09-08 16:06:37 +0300

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import styles from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
-    <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total: {total}</p>
-        <p>Positive feedback: {positivePercentage}%</p>
-    </div>
+    <div className={styles.statistics}>
+    <p className={styles.statItem}>Good: {good}</p>
+    <p className={styles.statItem}>Neutral: {neutral}</p>
+    <p className={styles.statItem}>Bad: {bad}</p>
+    <p className={styles.statItem}>Total: {total}</p>
+    <p className={styles.statItem}>Positive feedback: {positivePercentage}%</p>
+  </div>
 );
 
 Statistics.propTypes = {
